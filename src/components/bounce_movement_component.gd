9199fa487da229d9b001746793c9a47996b3a9ba
Tharmoth <tharmoth@gmail.com>
2024-04-27 10:48:57 -0500

    partial level ups

--- src/components/bounce_movement_component.gd
+++ src/components/bounce_movement_component.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node
 
 
 var _startingPosition : Vector2
@@ -6,7 +6,7 @@ var _tween : Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_startingPosition = position
+	_startingPosition = get_parent().position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,5 +16,5 @@ func _process(delta: float) -> void:
 	_tween = create_tween()
 	_tween.set_ease(Tween.EASE_IN_OUT)
 	_tween.set_trans(Tween.TRANS_SINE)
-	_tween.tween_property(self, "position:y", _startingPosition.y - 10, 1)
-	_tween.tween_property(self, "position:y", _startingPosition.y, 1)
+	_tween.tween_property(get_parent(), "position:y", _startingPosition.y - 10, 1)
+	_tween.tween_property(get_parent(), "position:y", _startingPosition.y, 1)
