5c34abb0e7304e71a929b994f35424da5b1a1346
Tharmoth <tharmoth@gmail.com>
2024-04-26 20:43:23 -0500

    added player sprite

--- src/Character.gd
+++ src/Character.gd
@@ -4,6 +4,7 @@ const SPEED = 300.0
 
 static var instance : Character
 var knockback : Vector2 = Vector2.ZERO
+var charge : int = 0
 
 func _enter_tree() -> void:
 	instance = self
@@ -14,15 +15,35 @@ func _ready() -> void:
 func pickup(area : Area2D) -> void:
 	area.get_parent().queue_free()
 	%Timer.seek(10)
+	charge += 1
+	var node_to_make_visible : Node2D
+	if (charge == 1):
+		node_to_make_visible = %Charge1
+	elif (charge == 2):
+		node_to_make_visible = %Charge2
+	elif (charge == 3):
+		node_to_make_visible = %Charge3
+	else:
+		return
+	
+	node_to_make_visible.visible = true
+	node_to_make_visible.modulate = Color.TRANSPARENT
+	var visible_tween = create_tween()
+	visible_tween.tween_property(node_to_make_visible, "modulate", Color.WHITE, .5)
+
+func _process(delta: float) -> void:
+	global_rotation = 0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	var movement : Vector2
+	%Fire.visible = direction.length()
 	if direction.length() > 0:
 		movement = direction * SPEED
 	else:
 		movement = Vector2.ZERO 
+		
 
 	velocity = movement + knockback
 
