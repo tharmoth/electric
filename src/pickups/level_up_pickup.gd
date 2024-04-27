aa31208891b81b2d7ab22bcb614d45188f0d44e3
Tharmoth <tharmoth@gmail.com>
2024-04-27 12:12:07 -0500

    added pickup delay

--- src/pickups/level_up_pickup.gd
+++ src/pickups/level_up_pickup.gd
@@ -7,7 +7,10 @@ func _ready() -> void:
 	add_to_group("LevelUpPickup")
 	modulate = Color.TRANSPARENT
 	var tween = create_tween()
-	tween.tween_property(self, "modulate", Color.WHITE, .5)
+	tween.tween_property(self, "modulate", Color.WHITE, 1)
+	tween.tween_callback(func():
+		%Area2D.monitoring = true
+		%Area2D.monitorable = true)
 
 func destroy():
 	var tween = create_tween()
