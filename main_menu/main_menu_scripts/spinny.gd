873b9db12ec839584516648c363dffd8195accbc
AnonymousFood <wkersh2@gmail.com>
2024-04-26 23:17:57 -0500

    Can start game from main menu

--- main_menu/main_menu_scripts/spinny.gd
+++ main_menu/main_menu_scripts/spinny.gd
@@ -4,6 +4,7 @@ signal turnedKnob
 
 var following := false
 const MAX_DIST := 14000
+const MAX_TURN := 6.28
 
 func _physics_process(delta: float) -> void:
 	var mouseDist := get_global_mouse_position().distance_squared_to($knob.global_position)
@@ -15,16 +16,15 @@ func _physics_process(delta: float) -> void:
 
 	if following:
 		var ang := get_global_mouse_position().angle_to_point($knob.global_position) - PI/2
-		print("ang: " + String.num(ang))
 		
 		var d : Vector2 = ($knob/knobPoint.position.rotated($knob.rotation))
 		var a = $middlePoint.position.angle_to(d)
-		print("a: " + String.num(a))
 		
 		var finalAng : float = remap(a, -PI, PI, 0, 100)
-		print("finalAng: " + String.num(finalAng))
 		
 		var fang : float = lerp_angle($knob.rotation, ang, 0.05)
 		$knob.rotation = clamp(fang, 0, 2*PI)
-		emit_signal("turnedKnob", finalAng)
+		
+		if $knob.rotation > MAX_TURN:
+			get_tree().create_timer(.5).timeout.connect(func(): emit_signal("turnedKnob"))
 		
