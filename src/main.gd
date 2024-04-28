ee5cabaca0c4910faa6b92084a3025a867b62098
Tharmoth <tharmoth@gmail.com>
2024-04-28 11:36:32 -0500

    edge zapp starts at 90 seconds

--- src/main.gd
+++ src/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var COOLDOWN := 3.0
 var timer := 0.0
+var edge_shock : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%TickTick.finished.connect(restart)
@@ -28,6 +29,14 @@ func _process(delta: float) -> void:
 		
 	if WorldTimer.instance.time_elapsed > 60:
 		amount_to_spawn += 1
+		
+	if WorldTimer.instance.time_elapsed > 90 and not edge_shock:
+		edge_shock = true
+		%EdgeHurtbox.visible = true
+		%EdgeHurtbox.monitoring = true
+		%EdgeHurtbox.monitorable = true
+		var tween = create_tween()
+		tween.tween_property(%EdgeParticles, "amount_ratio", 1, 3)
 	
 	for i in range(floori(WorldTimer.instance.get_minutes_elapsed() / 5)):
 		amount_to_spawn += 1
