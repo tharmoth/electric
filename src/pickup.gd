acbbe20ed9cb48d4736d637ef2151ed8cc7317a1
Tharmoth <tharmoth@gmail.com>
2024-04-28 11:26:18 -0500

    pickups don't get stuck on edges

--- src/pickup.gd
+++ src/pickup.gd
@@ -1,11 +1,16 @@
 class_name Pickup extends Node2D
 
+const SPEED : float = 100
+
 func _ready() -> void:
 	add_to_group("Pickup")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_rotation = 0
+	var distance = global_position.distance_to(Vector2.ZERO)
+	if distance > 450:
+		global_position = global_position.move_toward(Vector2.ZERO, SPEED * delta)
 
 func on_pickup():
 	queue_free()
