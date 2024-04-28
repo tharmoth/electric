bb3d214bd1e26b0d9762f699435cb0e0aa5db40d
AnonymousFood <wkersh2@gmail.com>
2024-04-28 17:08:03 -0500

    Updated sound dials

--- main_menu/sound_dial.gd
+++ main_menu/sound_dial.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 var following := false
-const MAX_DIST := 32000
-const MAX_TURN := 6.28
+const MAX_DIST := 42000
+const MAX_TURN := 2 * PI
+
+var last_angle = 0
+var grab : float = 0
+var started : bool = false
 @export var audioBus : int
 
 func _ready():
@@ -13,20 +17,23 @@ func _physics_process(_delta: float) -> void:
 	var knob_rot = $knob.rotation
 	
 	if mouseDist < MAX_DIST and Input.is_action_just_pressed("click"):
+		grab = get_global_mouse_position().angle_to_point($knob.global_position) - PI/2
 		following = true
 	
 	if Input.is_action_just_released("click"):
 		following = false
 	
 	if following:
-		var ang := get_global_mouse_position().angle_to_point($knob.global_position) - PI/2
+		var ang := get_global_mouse_position().angle_to_point($knob.global_position) - PI/2 - grab
 		var d : Vector2 = ($knob/knobPoint.position.rotated(knob_rot))
 		var a = $middlePoint.position.angle_to(d)
+		
 		var musicVol : float
 		if a < 0:
 			musicVol = remap(a, -PI, PI, -24, 24)
 		else:
 			musicVol = remap(a, 0, PI, 0, 10)
+			
 		var fang : float = lerp_angle(knob_rot, ang, 0.05)
 		$knob.rotation = clamp(fang, 0.1, 2*PI-0.1)
 		if $knob.rotation < 0.11:
