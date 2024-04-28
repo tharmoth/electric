0ad14c90a2397ad9f7dfb57d6eaa66e4c68e7d8d
AnonymousFood <wkersh2@gmail.com>
2024-04-28 13:30:07 -0500

    Finished shoulder laser

--- src/shoulder_laser.gd
+++ src/shoulder_laser.gd
@@ -3,16 +3,15 @@ extends Node2D
 func _on_reload_timeout() -> void:
 	$Reload.set_wait_time(8.0 / Character.instance.stats.shoulder_lasers)
 	var enemies = get_tree().get_nodes_in_group("Enemy")
-	print("boom! " + str($Reload.get_wait_time()))
 	for enemy in enemies:
 		var origin = global_position
 		var target = enemy.global_position
-		if origin.distance_to(target) < 1000:
+		if origin.distance_to(target) < 300:
+			$laserNoise.play()
 			var line = Line2D.new()
 			line.points = [origin, target]
-			print(str(origin) + " " + str(target))
 			line.width = 0
-			line.default_color = Color(10, 0, 0, 1)
+			line.default_color = Color(0, 0, 10, 1)
 			get_parent().get_tree().root.add_child(line)
 			
 			var laser_tween = get_parent().get_tree().create_tween()
